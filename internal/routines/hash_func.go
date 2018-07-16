0bb67e9b9fa855a144f4b1ffd93cbcb173ea46d0
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2018-07-16 14:46:01 +0300

    Improved the performance of the hash function a little bit

--- internal/routines/hash_func.go
+++ internal/routines/hash_func.go
@@ -12,7 +12,10 @@ import (
 
 const (
 	randomNumber         = uint64(4735311918715544114)
-	knuthsMultiplicative = 0x45d9f3b
+	knuthsMultiplicative8  = 179
+	knuthsMultiplicative16 = 47351
+	knuthsMultiplicative32 = 0x45d9f3b
+	//knuthsMultiplicative64 = 1442695040888963407
 )
 
 var (
@@ -29,12 +32,6 @@ func preHashPointer(in interface{}) uint64 {
 
 func preHash(keyI I.Key) (value uint64, typeId uint8) {
 	switch key := keyI.(type) {
-	case bool:
-		if key {
-			return 1, 0
-		} else {
-			return 0, 0
-		}
 	case string:
 		return preHashString(key), 1
 	case int:
@@ -48,55 +45,23 @@ func preHash(keyI I.Key) (value uint64, typeId uint8) {
 	case int16:
 		return uint64(key), 6
 	case uint16:
-		return uint64(key), 6
-	case int32:
 		return uint64(key), 7
-	case uint32:
+	case int32:
 		return uint64(key), 8
-	case int64:
+	case uint32:
 		return uint64(key), 9
-	case uint64:
+	case int64:
 		return uint64(key), 10
+	case uint64:
+		return uint64(key), 11
 	case float32:
-		return uint64(math.Float32bits(key)), 11
+		return uint64(math.Float32bits(key)), 12
 	case float64:
-		return uint64(math.Float64bits(key)), 12
+		return uint64(math.Float64bits(key)), 13
 	case complex64:
-		return uint64(math.Float32bits(real(key)) ^ math.Float32bits(imag(key))), 13
+		return uint64(math.Float32bits(real(key)) ^ math.Float32bits(imag(key))), 14
 	case complex128:
-		return uint64(math.Float64bits(real(key)) ^ math.Float64bits(imag(key))), 14
-	case *bool:
-		return preHashPointer(key), 32
-	case *string:
-		return preHashPointer(key), 33
-	case *int:
-		return preHashPointer(key), 34
-	case *uint:
-		return preHashPointer(key), 35
-	case *int8:
-		return preHashPointer(key), 36
-	case *uint8:
-		return preHashPointer(key), 37
-	case *int16:
-		return preHashPointer(key), 38
-	case *uint16:
-		return preHashPointer(key), 39
-	case *int32:
-		return preHashPointer(key), 40
-	case *uint32:
-		return preHashPointer(key), 41
-	case *int64:
-		return preHashPointer(key), 42
-	case *uint64:
-		return preHashPointer(key), 43
-	case *float32:
-		return preHashPointer(key), 44
-	case *float64:
-		return preHashPointer(key), 45
-	case *complex64:
-		return preHashPointer(key), 46
-	case *complex128:
-		return preHashPointer(key), 47
+		return uint64(math.Float64bits(real(key)) ^ math.Float64bits(imag(key))), 15
 	default:
 		return preHashString(fmt.Sprintf("%v", key)), 63
 	}
@@ -107,13 +72,13 @@ func HashFunc(blockSize int, key I.Key) int {
 	typeXorer := bits.RotateLeft64(randomNumber, int(typeId))
 	fullHash := preHashed ^ typeXorer
 	hash := uint64(0)
-	subHash1 := (fullHash >> 32) ^ (fullHash & 0xffffffff) ^ knuthsMultiplicative
-	hash ^= subHash1 * knuthsMultiplicative
-	subHash2 := (subHash1 >> 16) ^ (subHash1 & 0xffff) ^ knuthsMultiplicative
-	hash ^= subHash2 * knuthsMultiplicative
-	subHash3 := (subHash2 >> 8) ^ (subHash2 & 0xff) ^ knuthsMultiplicative
-	hash ^= subHash3 * knuthsMultiplicative
-	subHash4 := (subHash3 >> 4) ^ (subHash3 & 0x7) ^ knuthsMultiplicative
-	hash ^= subHash4 * knuthsMultiplicative
+	subHash1 := (fullHash >> 32) ^ (fullHash & 0xffffffff) ^ knuthsMultiplicative32
+	hash ^= uint64(uint32(subHash1) * knuthsMultiplicative32)
+	subHash2 := (subHash1 >> 16) ^ (subHash1 & 0xffff) ^ knuthsMultiplicative16
+	hash ^= uint64(uint16(subHash2) * knuthsMultiplicative16)
+	subHash3 := (subHash2 >> 8) ^ (subHash2 & 0xff) ^ knuthsMultiplicative8
+	hash ^= uint64(uint8(subHash3) * knuthsMultiplicative8)
+	subHash4 := (subHash3 >> 4) ^ (subHash3 & 0x7) ^ knuthsMultiplicative8
+	hash ^= uint64(uint8(subHash4) * knuthsMultiplicative8)
 	return int(hash) % blockSize
 }
